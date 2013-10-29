9af200fd7fa070a1ce43afe27e143554d34bc271
Mikael Kohlmyr <mikael.kohlmyr@triggeredmessaging.com>
2013-10-29 12:02:54 +0000

    version update

--- setup.py
+++ setup.py
@@ -1,3 +1,3 @@
 from distutils.core import setup
 
-setup(name='pyresponse', version='1.2.2', py_modules=['pyresponse'], requires=['suds'])
+setup(name='pyresponse', version='1.2.3', py_modules=['pyresponse'], requires=['suds'])
