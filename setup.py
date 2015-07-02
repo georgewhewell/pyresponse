08c1d44ff860ac4cf0e175dd78cfed22fe47110a
George Whewell <georgerw@gmail.com>
2015-07-02 15:28:02 +0100

    adds test stubs

--- setup.py
+++ setup.py
@@ -1,3 +1,28 @@
-from distutils.core import setup
+import sys
 
-setup(name='pyresponse', version='1.2.7', py_modules=['pyresponse'], requires=['suds'])
+from distutils.core import setup
+from setuptools.command.test import test as TestCommand
+
+
+class PyTest(TestCommand):
+    pytest_args = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        import pytest
+        errno = pytest.main(self.pytest_args)
+        sys.exit(errno)
+
+setup(
+    name='pyresponse',
+    version='1.2.7', 
+    py_modules=['pyresponse'],
+    requires=['suds'],
+    tests_require=['pytest'],
+    cmdclass={'test': PyTest},
+)
---
+++ tests/test_basic.py
@@ -0,0 +1,11 @@
+def test_import_client():
+    from pyresponse import PureResponseClient as PureResponseClient
+
+def test_initialize_client():
+    from pyresponse import PureResponseClient as PureResponseClient
+    pureresponse = PureResponseClient()
+
+def test_authentication():
+    from pyresponse import PureResponseClient as PureResponseClient
+    pureresponse = PureResponseClient()
+    pureresponse.authenticate('tm.system', 'crwkbplf')
