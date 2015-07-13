cfb88aea196597d63c98511b2aeeb3151dcd050a
George Whewell <georgerw@gmail.com>
2015-07-13 18:56:09 +0100

    add list tests

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,11 +1,21 @@
-def test_import_client():
-    from pyresponse import PureResponseClient as PureResponseClient
+import uuid
+from unittest import TestCase
 
-def test_initialize_client():
-    from pyresponse import PureResponseClient as PureResponseClient
-    pureresponse = PureResponseClient()
+from pyresponse import PureResponseClient as PureResponseClient
 
-def test_authentication():
-    from pyresponse import PureResponseClient as PureResponseClient
-    pureresponse = PureResponseClient()
-    pureresponse.authenticate('tm.system', 'crwkbplf')
+ListName = lambda: str(uuid.uuid4())
+
+
+class BasicTests(TestCase):
+    
+    def setUp(self):
+        self.client = PureResponseClient()
+        self.client.authenticate('tm.system', 'crwkbplf')
+
+    def test_create_list(self):
+        self.client.create_list(ListName(), 
+            {'email': 'blackhole@example.com', 'name': 'John Doe'})
+
+    def test_create_list_multiple(self):
+        self.client.create_list(ListName(), 
+            [{'email': 'mars@example.com', 'name': 'John Doe'}, {'email': 'venus@example.com', 'name': 'Jane Doe'}])
